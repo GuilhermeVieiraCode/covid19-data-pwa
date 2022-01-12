34ade356eeb02fb5418d34fb683e719c928e2891
Guilherme <gui.vieira.code@gmail.com>
2022-01-12 15:02:37 -0300

    Adiciona consulta por países com o componente Select

--- src/containers/Main/index.js
+++ src/containers/Main/index.js
@@ -2,25 +2,40 @@ import React, { memo, useState, useCallback, useEffect } from 'react';
 import Api from '../../services/api';
 import { ContainerStyled } from './style';
 import Board from './components/Board';
+import Panel from './components/Panel'
 
 function Main(){
     const [data, setData] = useState({});
     const [country, setCountry] = useState('brazil');
+    const updatedAt = new Date().toLocaleString();
 
     const getCovidData = useCallback((country) => {
-        Api.getCountry(country)
+        Api
+            .getCountry(country)
             .then(data => setData(data))
     }, []);
 
     useEffect(() => {
         getCovidData(country)
-    }, [getCovidData, country])
+    }, [getCovidData, country]);
+
+    const handleChange = ({ target }) => {
+        const country = target.value;
+        setCountry(country);
+    }
 
     return(
         <ContainerStyled>
             <div className='mb-2'>
-                <Board data={data}/>
+                <Panel
+                    data={data}
+                    updatedAt={updatedAt}
+                    onChange={handleChange}
+                    country={country}
+                    getCovidData={getCovidData}
+                />
             </div>
+            <Board data={data}/>
         </ContainerStyled>
     );
 };
