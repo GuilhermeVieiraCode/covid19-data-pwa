bbe852f1ad15656ac10b46023eddba8cfd17f8cb
Guilherme <gui.vieira.code@gmail.com>
2022-01-12 21:43:13 -0300

    Adiciona botão para compartilhar dados da interface

--- src/containers/Main/components/Panel.jsx
+++ src/containers/Main/components/Panel.jsx
@@ -1,13 +1,45 @@
 import React, { memo } from 'react';
-import RefreshIcon from '../../../assets/images/refresh.svg';
 import { Card, Typography, Button, Select, MenuItem } from '../../../components';
 import COUNTRIES from '../../../commons/constants/countries';
 import { CardPanelContentStyled, ItemStyled } from './style';
 
 const navigatorHasShare = navigator.share;
 
-function Panel({ updatedAt, onChange, data, country, getCovidData }){
+function Panel({ updatedAt, onChange, data, country }){
   const { cases, recovered, deaths, todayCases, todayDeaths } = data;
+
+  const textCovid19 = `País: ${country} -
+                       Dados atualizados em ${updatedAt} -
+                       Hoje - Casos: ${todayCases}. Óbitos: ${todayDeaths}.
+                       Total - Casos: ${cases}. Óbitos: ${deaths}. Recuperados: ${recovered}`;
+
+  const shareInfo = () => {
+      navigator.share({
+            title: `Dados do Covid-19 - ${country}`,
+            text: textCovid19,
+            url: '',
+      });
+  };
+
+  const renderShareButton = (
+      <div>
+          <Button variant="contained" color="primary" onClick={shareInfo}>
+              Compartilhar
+          </Button>
+      </div>
+  );
+
+  const copyInfo = () => {
+      navigator.clipboard.writeText(textCovid19);
+  };
+
+  const renderCopyButton = (
+      <div>
+          <Button variant="contained" color="primary" onClick={copyInfo}>
+              Copiar
+          </Button>
+      </div>
+  );
   
   const renderCountries = (country, index) => (
       <MenuItem key={`country-${index}`} value={country.value}>
@@ -22,7 +54,7 @@ function Panel({ updatedAt, onChange, data, country, getCovidData }){
       <Card>
           <CardPanelContentStyled>
               <div>
-                <Typography variant="h5" component="span" color="primary">COVID19</Typography>
+                <Typography variant="h5" component="span" color="primary">COVID-19</Typography>
                 <Typography variant="h6" component="p">Painel Coronavírus</Typography>
                 <Typography variant="body2" component="span" color="secondary">Atualizado em: {updatedAt}</Typography>
                 <div className="pt-2">
@@ -31,6 +63,7 @@ function Panel({ updatedAt, onChange, data, country, getCovidData }){
                     </Select>
                 </div>
             </div>
+            {navigatorHasShare ? renderShareButton : renderCopyButton}
           </CardPanelContentStyled>
       </Card>
   );
